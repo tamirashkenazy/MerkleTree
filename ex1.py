e6e2d1845509be8c82de2a965672429994af0455
Tamir Ashkenazy <Tamir_Ashkenazy@apple.com>
2020-05-01 12:59:53 +0300

    bug fixes of handlers, returns, getters

--- ex1.py
+++ ex1.py
@@ -1,8 +1,7 @@
 # tamir ashkenazy, 204508949, lidor alis, 201025160
-import sys
 from hashlib import sha256
 UTF_8_ENCODE = "utf-8"
-
+from termcolor import colored, cprint
 
 
 class Node(object):
@@ -26,6 +25,9 @@ class MerkleTree(object):
 
 
 def build_merkle_tree(str_list):
+    if not str_list:
+        return None
+
     nodes = [Node(leaf) for leaf in str_list]
     while len(nodes) != 1:
         left, right = nodes.pop(0), nodes.pop(0)
@@ -46,55 +48,70 @@ def go_out():
     return
 
 HANDLE_FUNCS_MAP = {
-    1 : build_merkle_tree,
-    2 : create_proof_of_inclusion,
-    3 : check_proof_of_inclusion,
-    4 : None
+    "1" : build_merkle_tree,
+    "2" : create_proof_of_inclusion,
+    "3" : check_proof_of_inclusion,
+    "4" : None
 }
 
 class ArgumentsHandler(object):
-    def __init__(self, args):
+    def __init__(self, args, is_merkle_tree=False):
         self.args = args
-        self.operation = args[0]
-        self.merkle_tree = None
+        self.operation = self.set_operation()
+        self.is_merkle_tree = is_merkle_tree
 
 
-    def set_operation(self, index):
-        self.operation = index
+    def set_operation(self):
+        if len(args) >= 1:
+            return args[0]
+        else:
+            return None
 
     def get_handler(self):
         if self.operation in HANDLE_FUNCS_MAP:
+            #
+            if self.operation == 2 and not self.is_merkle_tree:
+                return None
+
             return HANDLE_FUNCS_MAP[self.operation]
         else:
             return None
 
-    def set_merkle_tree(self, merkle_tree):
-        self.merkle_tree = merkle_tree
+    def get_args_for_function(self):
+        if len(self.args) >= 1:
+           return self.args[1:]
+        else:
+            return []
 
-    def get_merkle_tree(self):
-        return self.merkle_tree
+    def __repr__(self):
+        return self.args
 
-
-OPERATION_INDEX = 0
-EXIT_OPERATION = 5
+EXIT_OPERATION = "5"
 
 if __name__ == "__main__":
     try:
         args = input()
         args = args.split()
-        # args_handler = ArgumentsHandler(args)
-        operation = 0
-        if len(args) >= 1:
-            operation = args[OPERATION_INDEX]
+        args_handler = ArgumentsHandler(args)
+        operation = args_handler.operation
 
-        while operation != EXIT_OPERATION:
-            if operation in HANDLE_FUNCS_MAP:
-                HANDLE_FUNCS_MAP[operation](args[1:])
+        while operation != EXIT_OPERATION: # !=5
+            if operation == "1":
+                merkle_tree = build_merkle_tree(args_handler.get_args_for_function())
+                cprint(merkle_tree.root, "blue")
+                if merkle_tree:
+                    args_handler.is_merkle_tree = True
+
+            else:
+                handler_function = args_handler.get_handler() #None or function from dict
+                if handler_function:
+                    return_value = handler_function()
+                    # TODO - something with the return value
 
             args = input()
             args = args.split()
-            if len(args) > 1:
-                operation = args[OPERATION_INDEX]
+            args_handler = ArgumentsHandler(args)
+            operation = args_handler.set_operation()
 
     except Exception as e:
         print(e)
